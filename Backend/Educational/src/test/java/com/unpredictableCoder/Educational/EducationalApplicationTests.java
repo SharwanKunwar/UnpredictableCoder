10603c5a8a240dc9acba88b1a5b023d01553c0f5
SharwanKunwar <sharwankunwar07@gmail.com>
2025-10-05 08:34:13 +0545

    added backend and frontend project setup

---
+++ Backend/Educational/src/test/java/com/unpredictableCoder/Educational/EducationalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unpredictableCoder.Educational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
