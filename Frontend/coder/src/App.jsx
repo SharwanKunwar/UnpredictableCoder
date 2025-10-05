fdf3a268890376471f25f585bc2896bbaeb60278
SharwanKunwar <sharwankunwar07@gmail.com>
2025-10-05 08:55:54 +0545

    created frontend part with vite

---
+++ Frontend/coder/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-neutral-400 py-10'>
+      <h1 className='underline text-2xl text-white'>Comming soon!!!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
